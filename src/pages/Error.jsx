5e2e808ec9dfe03a14ca38bd003175315cc0aa38
visky-krisztina <127683091+visky-krisztina@users.noreply.github.com>
2024-01-14 20:56:57 +0200

    css changes

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,27 +3,27 @@ import img from "../assets/not-found.svg";
 import Wrapper from "../css/Error";
 
 const Error = () => {
-  const error = useRouteError();
-  console.log(error);
-  if (error.status === 404) {
-    return (
-      <Wrapper>
-        <div className="container">
-          <img src={img} alt="not found" />
-          <h3>Ohh! page not found</h3>
-          <p>We can't seem to find the page you're looking for</p>
-          <Link to="/">back home</Link>
-        </div>
-      </Wrapper>
-    );
-  }
-  return (
-    <Wrapper>
-      <div>
-        <h3>something went wrong</h3>
-      </div>
-    </Wrapper>
-  );
+	const error = useRouteError();
+	console.log(error);
+	if (error.status === 404) {
+		return (
+			<Wrapper>
+				<div className='error-container'>
+					<img src={img} className='error-img' alt='not found' />
+					<h3 className='error-h3'>Ohh! page not found</h3>
+					<p className='error-p'>We can't seem to find the page you're looking for</p>
+					<Link to='/'>back home</Link>
+				</div>
+			</Wrapper>
+		);
+	}
+	return (
+		<Wrapper>
+			<div>
+				<h3 className='error-h3'>something went wrong</h3>
+			</div>
+		</Wrapper>
+	);
 };
 
 export default Error;
